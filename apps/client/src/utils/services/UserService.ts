807b5e9cf95453140cd16c7bfad7c4549e14ed18
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-22 17:00:50 +0100

    add logout, register and me handlers

--- apps/client/src/utils/services/UserService.ts
+++ apps/client/src/utils/services/UserService.ts
@@ -1,15 +1,27 @@
-import { APIRoutes, LoginResponse, LoginUserInput } from "shared";
+import { APIRoutes, CreateUserInput, JwtPayloadUser, LoginResponse, LoginUserInput } from "shared";
 
 import { APIService } from "./APIService";
 
 export const UserService = {
   me: async () => {
-    return await APIService.get(APIRoutes.ME);
+    return (await APIService.get(APIRoutes.ME)) as JwtPayloadUser | null;
+  },
+
+  register: async (body: CreateUserInput) => {
+    return (await APIService.post(APIRoutes.USERS, {
+      body,
+    })) as { accessToken: string };
   },
 
   login: async (body: LoginUserInput) => {
     return (await APIService.post(APIRoutes.LOGIN, {
       body,
     })) as LoginResponse;
+  },
+
+  logout: async () => {
+    return (await APIService.post(APIRoutes.LOGOUT, {
+      body: {},
+    })) as boolean;
   },
 };
