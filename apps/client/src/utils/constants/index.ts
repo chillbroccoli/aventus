5a0c0d4e29b9dc53fa5f16f9644a31eec7514953
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-22 16:59:51 +0100

    add new constants for client

--- apps/client/src/utils/constants/index.ts
+++ apps/client/src/utils/constants/index.ts
@@ -1,6 +1,15 @@
 import { ClientRoutes } from "shared/constants";
 
-export const userNavigation = [
-  { name: "Your Profile", href: ClientRoutes.PROFILE },
+export const profileNav = [
   { name: "Settings", href: ClientRoutes.SETTINGS },
+  { name: "New Project", href: ClientRoutes.NEW_PROJECT },
+  { name: "Reading List", href: ClientRoutes.READING_LIST },
 ];
+
+export const tabsOptions = [
+  { value: "all", label: "All" },
+  { value: "latest", label: "Latest" },
+];
+
+export const newProjectTextEditorDefaultContent =
+  "<p>Example structure...</p><p>(Not all points need to be met, just an example structure)</p><ol><li><p>Project Instructions</p></li><li><p>User Stories</p></li><li><p>Ideas, for example, this [package / library / framework] might be useful for this project</p></li><li><p>Helpful Links</p></li><li><p>Example Projects</p></li></ol>";
