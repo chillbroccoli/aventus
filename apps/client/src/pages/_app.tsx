d7344a63700ee1b0512ef00c22d0f2845a8f6230
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-22 22:28:16 +0100

    add poppins font

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import { Poppins } from "@next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppProps } from "next/app";
 import Head from "next/head";
@@ -7,6 +8,11 @@ import Head from "next/head";
 const queryClient = new QueryClient();
 
 import { GlobalStyles } from "@/styles/global";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -21,6 +27,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           theme={{
             primaryColor: "teal",
             colorScheme: "light",
+            fontFamily: poppins.style.fontFamily,
+            headings: { fontFamily: poppins.style.fontFamily },
           }}
         >
           <GlobalStyles />
