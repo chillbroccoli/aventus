f262d6d943a1b714822944e4eb87cc82e838ffc1
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-22 22:29:28 +0100

    update NewProject view

--- apps/client/src/views/NewProject.tsx
+++ apps/client/src/views/NewProject.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Flex, TextInput } from "@mantine/core";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button, Container, Flex } from "@mantine/core";
 import { Link } from "@mantine/tiptap";
 import Highlight from "@tiptap/extension-highlight";
 import SubScript from "@tiptap/extension-subscript";
@@ -7,13 +8,23 @@ import TextAlign from "@tiptap/extension-text-align";
 import Underline from "@tiptap/extension-underline";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { FormProvider, useForm } from "react-hook-form";
+import { CreateProjectInput, createProjectSchema } from "shared";
 
+import { Input } from "@/components/atoms/Input";
 import { TagSelect } from "@/components/atoms/TagSelect";
 import { MainLayout } from "@/components/layouts/MainLayout";
 import { TextEditor } from "@/components/molecules/TextEditor";
 import { newProjectTextEditorDefaultContent } from "@/utils/constants";
 
 export function NewProjectView() {
+  const methods = useForm<CreateProjectInput>({
+    resolver: zodResolver(createProjectSchema),
+    defaultValues: {
+      content: newProjectTextEditorDefaultContent,
+    },
+  });
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -25,18 +36,38 @@ export function NewProjectView() {
       TextAlign.configure({ types: ["heading", "paragraph"] }),
     ],
     content: newProjectTextEditorDefaultContent,
+    onUpdate({ editor }) {
+      methods.setValue("content", editor.getHTML());
+    },
   });
+
+  const onSubmit = (data: CreateProjectInput) => {
+    return data;
+  };
 
   return (
     <MainLayout>
       <Container size="lg" mt={20}>
         <Box mx="auto" w="75%">
           <Flex direction="column">
-            <TextInput placeholder="Project's title" size="xl" w="100%" mb={10} />
-            <Box my={10}>
-              <TagSelect />
-            </Box>
-            <TextEditor editor={editor} />
+            <FormProvider {...methods}>
+              <form onSubmit={methods.handleSubmit(onSubmit)}>
+                <Box mb={10}>
+                  <Input.Text name="title" placeholder="Project's title" size="xl" w="100%" />
+                </Box>
+                <Box my={10}>
+                  <TagSelect />
+                </Box>
+                <Box my={10}>
+                  <Input.Textarea name="description" placeholder="Description" size="xl" />
+                </Box>
+                <TextEditor editor={editor} />
+
+                <Flex justify="end" align="center" mt={30}>
+                  <Button type="submit">Create New Project</Button>
+                </Flex>
+              </form>
+            </FormProvider>
           </Flex>
         </Box>
       </Container>
