a575f5041261b8cb8d7c2d22aed190d37fe6adcf
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-22 22:28:32 +0100

    remove not needed constant, add new profile nav link

--- apps/client/src/utils/constants/index.ts
+++ apps/client/src/utils/constants/index.ts
@@ -3,12 +3,8 @@ import { ClientRoutes } from "shared/constants";
 export const profileNav = [
   { name: "Settings", href: ClientRoutes.SETTINGS },
   { name: "New Project", href: ClientRoutes.NEW_PROJECT },
+  { name: "My Projects", href: ClientRoutes.MY_PROJECTS },
   { name: "Reading List", href: ClientRoutes.READING_LIST },
-];
-
-export const tabsOptions = [
-  { value: "all", label: "All" },
-  { value: "latest", label: "Latest" },
 ];
 
 export const newProjectTextEditorDefaultContent =
