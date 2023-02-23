68ac61229afaafc63738b691a3d2bc5cbff9ae16
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-23 12:38:45 +0100

    fetch all projects and display ProjectsFeed

--- apps/client/src/views/Home.tsx
+++ apps/client/src/views/Home.tsx
@@ -1,10 +1,17 @@
 import { Container, Grid } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
 
 import { MainLayout } from "@/components/layouts/MainLayout";
 import { Tags } from "@/components/molecules/Tags";
 import { ProjectsFeed } from "@/components/organisms/ProjectsFeed";
+import { ProjectService } from "@/utils/services/ProjectService";
 
 export function HomeView() {
+  const { data } = useQuery({
+    queryKey: ["projects"],
+    queryFn: ProjectService.findAll,
+  });
+
   return (
     <MainLayout>
       <Container size="lg" mt={20}>
@@ -12,9 +19,7 @@ export function HomeView() {
           <Grid.Col span={3}>
             <Tags />
           </Grid.Col>
-          <Grid.Col span={7}>
-            <ProjectsFeed />
-          </Grid.Col>
+          <Grid.Col span={7}>{data && <ProjectsFeed data={data} />}</Grid.Col>
           <Grid.Col span={2}>3</Grid.Col>
         </Grid>
       </Container>
