6e7f86ca8ceb33a19e5b989766b8f479bd123881
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-23 12:37:05 +0100

    add paramsWithSlugSchema

--- packages/shared/schemas/params.ts
+++ packages/shared/schemas/params.ts
@@ -4,4 +4,9 @@ export const paramsWithIdSchema = z.object({
   id: z.string(),
 });
 
+export const paramsWithSlugSchema = z.object({
+  slug: z.string(),
+});
+
 export type ParamsWithId = z.infer<typeof paramsWithIdSchema>;
+export type ParamsWithSlug = z.infer<typeof paramsWithSlugSchema>;
