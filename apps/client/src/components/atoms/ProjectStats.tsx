f25b45be7fbc5a83a274e3bca0e8f0b996d7ef1e
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-24 21:51:24 +0100

    display likes and comments count, add like mutation

--- apps/client/src/components/atoms/ProjectStats.tsx
+++ apps/client/src/components/atoms/ProjectStats.tsx
@@ -1,15 +1,50 @@
 import { ActionIcon, Flex, Text } from "@mantine/core";
-import { IconBookmark, IconHeart, IconMessage2 } from "@tabler/icons";
+import { IconBookmark, IconHeart, IconHeartFilled, IconMessage2 } from "@tabler/icons-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
-export function ProjectStats({ comments }: { comments?: number }) {
+import { useMe } from "@/hooks/useMe";
+import { ProjectService } from "@/utils/services/ProjectService";
+
+export function ProjectStats() {
+  const {
+    query: { slug },
+  } = useRouter();
+
+  const { me } = useMe();
+
+  const queryClient = useQueryClient();
+
+  const { data } = useQuery({
+    queryKey: ["projectStats", slug],
+    queryFn: () => ProjectService.getProjectStats(slug as string),
+  });
+
+  const { mutate: like } = useMutation({
+    mutationKey: ["likeProject", slug],
+    mutationFn: () => ProjectService.likeProject(slug as string),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["projectStats", slug]);
+    },
+  });
+
+  const isLiked = data?.likes?.find((like) => like.userId === me?.id);
+
   return (
     <Flex direction="column" gap={20}>
       <Flex direction="column" align="center" justify="center">
-        <ActionIcon>
-          <IconHeart size={26} />
-        </ActionIcon>
+        {isLiked ? (
+          <ActionIcon color="red.5" onClick={() => like()}>
+            <IconHeartFilled size={26} />
+          </ActionIcon>
+        ) : (
+          <ActionIcon onClick={() => like()}>
+            <IconHeart size={26} />
+          </ActionIcon>
+        )}
+
         <Text fz="sm" color="gray.7">
-          0
+          {data?._count?.likes ?? 0}
         </Text>
       </Flex>
       <Flex direction="column" align="center" justify="center">
@@ -17,7 +52,7 @@ export function ProjectStats({ comments }: { comments?: number }) {
           <IconMessage2 size={26} />
         </ActionIcon>
         <Text fz="sm" color="gray.7">
-          {comments ?? 0}
+          {data?._count?.comments ?? 0}
         </Text>
       </Flex>
       <Flex direction="column" align="center" justify="center">
