c2a196450f445fcdf30358234424a7326cc8d508
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-24 21:59:06 +0100

    move comment to CommentCard component, showNotification after creating comment and invalidate projectStats

--- apps/client/src/components/organisms/Comments.tsx
+++ apps/client/src/components/organisms/Comments.tsx
@@ -1,8 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ActionIcon, Box, Button, createStyles, Divider, Flex, Text, Title } from "@mantine/core";
-import { IconDots } from "@tabler/icons";
+import { Box, Button, Divider, Flex, Title } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import { FormProvider, useForm } from "react-hook-form";
 import { CreateCommentInput, createCommentSchema } from "shared";
@@ -12,6 +11,7 @@ import { CommentService } from "@/utils/services/CommentService";
 
 import { Avatar } from "../atoms/Avatar";
 import { Input } from "../atoms/Input";
+import { CommentCard } from "../molecules/CommentCard";
 
 export function Comments() {
   const {
@@ -19,8 +19,6 @@ export function Comments() {
   } = useRouter();
 
   const { me } = useMe();
-
-  const { classes } = styles();
 
   const methods = useForm<CreateCommentInput>({
     resolver: zodResolver(createCommentSchema),
@@ -37,7 +35,12 @@ export function Comments() {
     mutationKey: ["createComment", slug],
     mutationFn: (input: CreateCommentInput) => CommentService.create(slug as string, input),
     onSuccess: () => {
+      showNotification({
+        title: "Comment created",
+        message: "Your comment has been created",
+      });
       methods.reset();
+      queryClient.invalidateQueries(["projectStats", slug]);
       queryClient.invalidateQueries(["comments", slug]);
     },
   });
@@ -69,48 +72,13 @@ export function Comments() {
         </Box>
       </Flex>
 
-      <Divider my={20} />
+      {data && data.length > 0 && <Divider my={20} />}
 
       <Box>
         <Flex direction="column">
-          {data &&
-            data.map((comment) => (
-              <Flex key={comment.id} align="start" justify="start" mt={10}>
-                <Box>
-                  <Avatar src={comment.user?.avatar} color="teal" alt="Avatar" />
-                </Box>
-                <Box p={10} ml={15} w="100%" className={classes.comment}>
-                  <Flex justify="space-between">
-                    <Flex align="end">
-                      <Text transform="capitalize" color="gray.8">
-                        {comment.user.name}
-                      </Text>
-                      <Text ml={10} color="gray.6" fz="sm" fw={300}>
-                        {dayjs(comment.createdAt).format("MMM DD, YYYY")}
-                      </Text>
-                    </Flex>
-                    {comment.user.id === me?.id && (
-                      <ActionIcon>
-                        <IconDots size={18} />
-                      </ActionIcon>
-                    )}
-                  </Flex>
-
-                  <Text mt={20} fw={400} color="gray.9">
-                    {comment.content}
-                  </Text>
-                </Box>
-              </Flex>
-            ))}
+          {data && data.map((comment) => <CommentCard key={comment.id} comment={comment} />)}
         </Flex>
       </Box>
     </Box>
   );
 }
-
-const styles = createStyles((theme) => ({
-  comment: {
-    border: `1px solid ${theme.colors.gray[3]}`,
-    boxShadow: theme.shadows.sm,
-  },
-}));
