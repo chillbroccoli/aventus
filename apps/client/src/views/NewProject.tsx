1e047e9c0f0c426cb9863a7fd284ee5100ed0830
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-24 21:53:31 +0100

    add creating project mutation

--- apps/client/src/views/NewProject.tsx
+++ apps/client/src/views/NewProject.tsx
@@ -1,6 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Button, Container, Flex } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import { Link } from "@mantine/tiptap";
+import { useMutation } from "@tanstack/react-query";
 import Highlight from "@tiptap/extension-highlight";
 import SubScript from "@tiptap/extension-subscript";
 import Superscript from "@tiptap/extension-superscript";
@@ -8,16 +10,34 @@ import TextAlign from "@tiptap/extension-text-align";
 import Underline from "@tiptap/extension-underline";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useRouter } from "next/router";
 import { FormProvider, useForm } from "react-hook-form";
-import { CreateProjectInput, createProjectSchema } from "shared";
+import { ClientRoutes, CreateProjectInput, createProjectSchema } from "shared";
 
 import { Input } from "@/components/atoms/Input";
 import { TagSelect } from "@/components/atoms/TagSelect";
 import { MainLayout } from "@/components/layouts/MainLayout";
 import { TextEditor } from "@/components/molecules/TextEditor";
 import { newProjectTextEditorDefaultContent } from "@/utils/constants";
+import { ProjectService } from "@/utils/services/ProjectService";
+import { RoutingService } from "@/utils/services/RoutingService";
 
 export function NewProjectView() {
+  const router = useRouter();
+
+  const { mutateAsync } = useMutation({
+    mutationKey: ["createProject"],
+    mutationFn: ProjectService.create,
+    onSuccess: (data) => {
+      showNotification({
+        title: "Project created",
+        message: "Your project has been created successfully",
+      });
+
+      router.push(RoutingService.getInterpolatedRoute([ClientRoutes.PROJECT, { slug: data.slug }]));
+    },
+  });
+
   const methods = useForm<CreateProjectInput>({
     resolver: zodResolver(createProjectSchema),
     defaultValues: {
@@ -41,13 +61,13 @@ export function NewProjectView() {
     },
   });
 
-  const onSubmit = (data: CreateProjectInput) => {
-    return data;
+  const onSubmit = async (input: CreateProjectInput) => {
+    await mutateAsync(input);
   };
 
   return (
     <MainLayout>
-      <Container size="lg" mt={20}>
+      <Container size="lg" mt={20} pb={20}>
         <Box mx="auto" w="75%">
           <Flex direction="column">
             <FormProvider {...methods}>
@@ -64,7 +84,9 @@ export function NewProjectView() {
                 <TextEditor editor={editor} />
 
                 <Flex justify="end" align="center" mt={30}>
-                  <Button type="submit">Create New Project</Button>
+                  <Button type="submit" disabled={!methods.formState.isValid}>
+                    Create New Project
+                  </Button>
                 </Flex>
               </form>
             </FormProvider>
