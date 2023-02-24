2ae5fb62201bf39b2acfa8169d010695084ff4b4
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-24 21:58:18 +0100

    update schemas adding likes and bookmarks fields

--- packages/shared/schemas/project.ts
+++ packages/shared/schemas/project.ts
@@ -5,14 +5,14 @@ import { userResponseSchema } from "./user";
 export const createProjectSchema = z.object({
   title: z
     .string({ required_error: "Title is required" })
-    .min(1, "Title must be at least 1 character long"),
+    .min(6, "Title must be at least 6 characters long"),
   description: z
     .string({ required_error: "Description is required" })
-    .min(1, "Description must be at least 1 character long"),
+    .min(50, "Description must be at least 50 characters long"),
   tags: z.array(z.string()).min(1, "At least 1 tag is required"),
   content: z
     .string({ required_error: "Content is required" })
-    .min(1, "Content must be at least 1 character long"),
+    .min(200, "Content must be at least 200 characters long"),
 });
 
 export const projectResponseSchema = z.object({
@@ -22,6 +22,8 @@ export const projectResponseSchema = z.object({
   description: z.string(),
   content: z.string(),
   tags: z.array(z.object({ id: z.number(), name: z.string() })),
+  likes: z.array(z.object({ id: z.number(), userId: z.number(), projectId: z.number() })),
+  bookmarks: z.array(z.object({ id: z.number(), userId: z.number(), projectId: z.number() })),
   user: userResponseSchema,
   createdAt: z.string(),
   updatedAt: z.string(),
@@ -30,5 +32,16 @@ export const projectResponseSchema = z.object({
   }),
 });
 
+export const projectStatsSchema = z.object({
+  likes: z.array(z.object({ id: z.number(), userId: z.number(), projectId: z.number() })),
+  bookmarks: z.array(z.object({ id: z.number(), userId: z.number(), projectId: z.number() })),
+  _count: z.object({
+    comments: z.number(),
+    likes: z.number(),
+    bookmarks: z.number(),
+  }),
+});
+
 export type CreateProjectInput = z.infer<typeof createProjectSchema>;
 export type ProjectResponse = z.infer<typeof projectResponseSchema>;
+export type ProjectStatsResponse = z.infer<typeof projectStatsSchema>;
