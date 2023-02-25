e06e6a3de5e498effeb1d821595072858e3ff5fa
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-25 22:05:43 +0100

    add keys constants

---
+++ apps/client/src/utils/constants/keys.ts
@@ -0,0 +1,18 @@
+export enum QUERY_KEYS {
+  PROJECT = "project",
+  PROJECTS = "projects",
+  PROJECT_STATS = "projectStats",
+  TAGS = "tags",
+  COMMENTS = "comments",
+}
+
+export enum MUTATION_KEYS {
+  LIKE_PROJECT = "likeProject",
+  BOOKMARK_PROJECT = "bookmarkProject",
+  CREATE_PROJECT = "createProject",
+  CREATE_COMMENT = "createComment",
+  DELETE_COMMENT = "deleteComment",
+  LOGOUT = "logout",
+  LOGIN = "login",
+  REGISTER = "register",
+}
