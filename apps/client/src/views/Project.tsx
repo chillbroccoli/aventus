1885626dbdae54dea99c663adc4139783719b86e
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-25 22:06:16 +0100

    move Project fetching to ProjectContent

--- apps/client/src/views/Project.tsx
+++ apps/client/src/views/Project.tsx
@@ -1,24 +1,10 @@
 import { Container, Grid } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/router";
 
 import { ProjectStats } from "@/components/atoms/ProjectStats";
 import { MainLayout } from "@/components/layouts/MainLayout";
 import { ProjectContent } from "@/components/organisms/ProjectContent";
-import { ProjectService } from "@/utils/services/ProjectService";
 
 export function ProjectView() {
-  const {
-    query: { slug },
-    isReady,
-  } = useRouter();
-
-  const { data } = useQuery({
-    queryKey: ["project", slug],
-    queryFn: () => ProjectService.findOne(slug as string),
-    enabled: isReady,
-  });
-
   return (
     <MainLayout>
       <Container size="lg" mt={20} pb={20}>
@@ -26,7 +12,9 @@ export function ProjectView() {
           <Grid.Col span={1}>
             <ProjectStats />
           </Grid.Col>
-          <Grid.Col span={9}>{data && <ProjectContent project={data} />}</Grid.Col>
+          <Grid.Col span={9}>
+            <ProjectContent />
+          </Grid.Col>
           <Grid.Col span={2}>3</Grid.Col>
         </Grid>
       </Container>
