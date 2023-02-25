597e868b80c27414e44bf5cf74736a9142a5905a
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-25 22:21:45 +0100

    move projects fetching to ProjectsFeed

--- apps/client/src/views/Home.tsx
+++ apps/client/src/views/Home.tsx
@@ -1,18 +1,10 @@
 import { Container, Grid } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
 
 import { MainLayout } from "@/components/layouts/MainLayout";
 import { Tags } from "@/components/molecules/Tags";
 import { ProjectsFeed } from "@/components/organisms/ProjectsFeed";
-import { QUERY_KEYS } from "@/utils/constants";
-import { ProjectService } from "@/utils/services/ProjectService";
 
 export function HomeView() {
-  const { data } = useQuery({
-    queryKey: [QUERY_KEYS.PROJECTS],
-    queryFn: ProjectService.findAll,
-  });
-
   return (
     <MainLayout>
       <Container size="lg" mt={20}>
@@ -20,7 +12,9 @@ export function HomeView() {
           <Grid.Col span={3}>
             <Tags />
           </Grid.Col>
-          <Grid.Col span={7}>{data && <ProjectsFeed data={data} />}</Grid.Col>
+          <Grid.Col span={7}>
+            <ProjectsFeed />
+          </Grid.Col>
           <Grid.Col span={2}>3</Grid.Col>
         </Grid>
       </Container>
