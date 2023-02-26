3242c51f1409f4453e47f42587c04daffa0b7fba
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-26 18:18:22 +0100

    make TextEditor a dynamic import

--- apps/client/src/views/NewProject.tsx
+++ apps/client/src/views/NewProject.tsx
@@ -1,15 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Button, Container, Flex } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
-import { Link } from "@mantine/tiptap";
 import { useMutation } from "@tanstack/react-query";
-import Highlight from "@tiptap/extension-highlight";
-import SubScript from "@tiptap/extension-subscript";
-import Superscript from "@tiptap/extension-superscript";
-import TextAlign from "@tiptap/extension-text-align";
-import Underline from "@tiptap/extension-underline";
-import { useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { FormProvider, useForm } from "react-hook-form";
 import { ClientRoutes, CreateProjectInput, createProjectSchema } from "shared";
@@ -17,10 +10,16 @@ import { ClientRoutes, CreateProjectInput, createProjectSchema } from "shared";
 import { Input } from "@/components/atoms/Input";
 import { TagSelect } from "@/components/atoms/TagSelect";
 import { MainLayout } from "@/components/layouts/MainLayout";
-import { TextEditor } from "@/components/molecules/TextEditor";
-import { MUTATION_KEYS, newProjectTextEditorDefaultContent } from "@/utils/constants";
+import { MUTATION_KEYS } from "@/utils/constants";
 import { ProjectService } from "@/utils/services/ProjectService";
 import { RoutingService } from "@/utils/services/RoutingService";
+
+const TextEditor = dynamic(
+  () => import("../components/molecules/TextEditor").then((mod) => mod.TextEditor),
+  {
+    ssr: false,
+  }
+);
 
 export function NewProjectView() {
   const router = useRouter();
@@ -41,23 +40,7 @@ export function NewProjectView() {
   const methods = useForm<CreateProjectInput>({
     resolver: zodResolver(createProjectSchema),
     defaultValues: {
-      content: newProjectTextEditorDefaultContent,
-    },
-  });
-
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Underline,
-      Link,
-      Superscript,
-      SubScript,
-      Highlight,
-      TextAlign.configure({ types: ["heading", "paragraph"] }),
-    ],
-    content: newProjectTextEditorDefaultContent,
-    onUpdate({ editor }) {
-      methods.setValue("content", editor.getHTML());
+      content: "",
     },
   });
 
@@ -79,14 +62,19 @@ export function NewProjectView() {
                   <TagSelect />
                 </Box>
                 <Box my={10}>
-                  <Input.Textarea name="description" placeholder="Description" size="xl" />
+                  <Input.Textarea
+                    name="description"
+                    placeholder="Description"
+                    size="xl"
+                    minRows={6}
+                  />
                 </Box>
-                <TextEditor editor={editor} />
+                <Box>
+                  <TextEditor name="content" />
+                </Box>
 
                 <Flex justify="end" align="center" mt={30}>
-                  <Button type="submit" disabled={!methods.formState.isValid}>
-                    Create New Project
-                  </Button>
+                  <Button type="submit">Create New Project</Button>
                 </Flex>
               </form>
             </FormProvider>
