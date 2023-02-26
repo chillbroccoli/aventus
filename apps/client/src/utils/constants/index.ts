7ee1805ae39057ca4216932bacfc1c97ef9b6d0f
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-26 18:18:50 +0100

    remove not needed constant

--- apps/client/src/utils/constants/index.ts
+++ apps/client/src/utils/constants/index.ts
@@ -7,7 +7,4 @@ export const profileNav = [
   { name: "Reading List", href: ClientRoutes.READING_LIST },
 ];
 
-export const newProjectTextEditorDefaultContent =
-  "<p>Example structure...</p><p>(Not all points need to be met, just an example structure)</p><ol><li><p>Project Instructions</p></li><li><p>User Stories</p></li><li><p>Ideas, for example, this [package / library / framework] might be useful for this project</p></li><li><p>Helpful Links</p></li><li><p>Example Projects</p></li></ol>";
-
 export * from "./keys";
