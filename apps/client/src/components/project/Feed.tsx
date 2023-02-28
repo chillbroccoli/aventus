afbde8e34d570c2560f0414574986a9d4e0a1910
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-28 14:18:10 +0100

    move data fetching outside Feed, display EmptyState

--- apps/client/src/components/project/Feed.tsx
+++ apps/client/src/components/project/Feed.tsx
@@ -1,19 +1,20 @@
 import { Box, Flex } from "@mantine/core";
+import { ProjectResponse } from "shared";
 
+import { EmptyState } from "@/components/project/EmptyState";
 import { ProjectCard } from "@/components/project/ProjectCard";
-import { api } from "@/utils/api";
 
-export function Feed() {
-  const { data } = api.project.useAll();
-
+export function Feed({ data }: { data?: ProjectResponse[] }) {
   return (
     <Box>
       <Flex direction="column" gap={20}>
         {data &&
+          data.length > 0 &&
           data.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
       </Flex>
+      {!data?.length && <EmptyState />}
     </Box>
   );
 }
