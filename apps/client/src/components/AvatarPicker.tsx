6256e2b41febb23207b8b515dcf5515c327a0bee
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-01 00:49:39 +0100

    export AvatarPicker to its own component

---
+++ apps/client/src/components/AvatarPicker.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, Flex, Image, Popover } from "@mantine/core";
+import { useState } from "react";
+
+import { generateRandomAvatar } from "@/utils/helpers/generateRandomAvatar";
+
+export function AvatarPicker({
+  changeAvatar,
+}: {
+  changeAvatar: (url: string) => void;
+}) {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState(generateRandomAvatar());
+
+  return (
+    <Popover
+      opened={isPopoverOpen}
+      onChange={setIsPopoverOpen}
+      position="top"
+      width={450}
+      shadow="sm"
+    >
+      <Popover.Target>
+        <Box>
+          <Button
+            variant="light"
+            fullWidth
+            onClick={() => setIsPopoverOpen(true)}
+          >
+            Pick Avatar
+          </Button>
+        </Box>
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Flex direction="column">
+          <Box w="100%" py={10}>
+            <Image src={avatarUrl} alt="Avatar" height={300} />
+          </Box>
+          <Flex gap={10}>
+            <Button
+              fullWidth
+              onClick={() => {
+                changeAvatar(avatarUrl);
+                setAvatarUrl(generateRandomAvatar());
+                setIsPopoverOpen(false);
+              }}
+            >
+              Looks great!
+            </Button>
+            <Button
+              variant="outline"
+              color="gray"
+              fullWidth
+              onClick={() => setAvatarUrl(generateRandomAvatar())}
+            >
+              Generate new one
+            </Button>
+          </Flex>
+        </Flex>
+      </Popover.Dropdown>
+    </Popover>
+  );
+}
--- apps/client/src/forms/RegisterForm.tsx
+++ apps/client/src/forms/RegisterForm.tsx
@@ -1,19 +1,15 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Flex, Image, Popover } from "@mantine/core";
+import { Box, Button, Flex } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { ClientRoutes, CreateUserInput, createUserSchema } from "shared";
 
+import { AvatarPicker } from "@/components/AvatarPicker";
 import { Input } from "@/components/Input";
 import { api } from "@/utils/api";
-import { generateRandomAvatar } from "@/utils/helpers/generateRandomAvatar";
 
 export function RegisterForm() {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  const [avatarUrl, setAvatarUrl] = useState(generateRandomAvatar());
-
   const router = useRouter();
 
   const { mutateAsync } = api.user.useRegister({
@@ -48,52 +44,9 @@ export function RegisterForm() {
           <Box>
             <Input.Text type="text" name="avatar" label="Avatar (URL)" />
           </Box>
-          <Popover
-            opened={isPopoverOpen}
-            onChange={setIsPopoverOpen}
-            position="top"
-            width={450}
-            shadow="sm"
-          >
-            <Popover.Target>
-              <Box>
-                <Button
-                  variant="light"
-                  fullWidth
-                  onClick={() => setIsPopoverOpen(true)}
-                >
-                  Pick Avatar
-                </Button>
-              </Box>
-            </Popover.Target>
-            <Popover.Dropdown>
-              <Flex direction="column">
-                <Box w="100%" py={10}>
-                  <Image src={avatarUrl} alt="Avatar" height={300} />
-                </Box>
-                <Flex gap={10}>
-                  <Button
-                    fullWidth
-                    onClick={() => {
-                      methods.setValue("avatar", avatarUrl);
-                      setAvatarUrl(generateRandomAvatar());
-                      setIsPopoverOpen(false);
-                    }}
-                  >
-                    Looks great!
-                  </Button>
-                  <Button
-                    variant="outline"
-                    color="gray"
-                    fullWidth
-                    onClick={() => setAvatarUrl(generateRandomAvatar())}
-                  >
-                    Generate new one
-                  </Button>
-                </Flex>
-              </Flex>
-            </Popover.Dropdown>
-          </Popover>
+          <AvatarPicker
+            changeAvatar={(url: string) => methods.setValue("avatar", url)}
+          />
           <Box>
             <Input.Password name="password" label="Password" required />
           </Box>
