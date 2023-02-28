d570826382a043f7d27be037a4210eca29da3a8b
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-28 14:18:43 +0100

    move author details to AuthorCard component

--- apps/client/src/components/project/Project.tsx
+++ apps/client/src/components/project/Project.tsx
@@ -7,50 +7,34 @@ import {
   Title,
   TypographyStylesProvider,
 } from "@mantine/core";
-import dayjs from "dayjs";
 import { ProjectResponse } from "shared";
 
-import { Avatar } from "@/components/Avatar";
 import { Comments } from "@/components/comment/Comments";
+import { useMeStore } from "@/utils/stores/useMeStore";
+
+import { SettingsMenu } from "./SettingsMenu";
 
 export function Project({ project }: { project?: ProjectResponse }) {
   const { classes } = styles();
 
+  const me = useMeStore((state) => state.me);
+
   if (!project) return null;
 
-  const { title, description, content, tags, user, updatedAt } = project;
+  const { title, description, content, tags, user } = project;
 
   return (
     <Box px={40} p={10} className={classes.main}>
-      <Flex>
-        <Box>
-          <Avatar
-            mr={6}
-            size="lg"
-            src={user?.avatar}
-            color="teal"
-            alt="Avatar"
-            className={classes.avatar}
-          />
-        </Box>
-        <Flex direction="column">
-          <Text transform="capitalize" fw={500} color="gray.8">
-            {user.name}
-          </Text>
-          <Text fz="xs" fw={300} color="gray.6">
-            {dayjs(updatedAt).format("MMM DD, YYYY")}
-          </Text>
-        </Flex>
-      </Flex>
+      {user.id === me?.id && <SettingsMenu />}
 
       <Flex direction="column" mt={14}>
         <Box>
-          <Title order={3} fw={600}>
+          <Title order={1} fw={600}>
             {title}
           </Title>
         </Box>
 
-        <Flex mt={8} gap={10}>
+        <Flex mt={10} gap={10}>
           {tags.map((tag) => (
             <Badge key={tag.id} variant="outline" radius="md">
               # {tag.name}
@@ -87,5 +71,6 @@ const styles = createStyles((theme) => ({
     borderRadius: theme.radius.sm,
     boxShadow: theme.shadows.sm,
     backgroundColor: theme.white,
+    position: "relative",
   },
 }));
