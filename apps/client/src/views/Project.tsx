94a36dbdbd5a701f3bb3b75dfcfc84b34265ca37
Kacper Malek <kacper.malek0828@gmail.com>
2023-02-28 22:00:35 +0100

    show loading state

--- apps/client/src/views/Project.tsx
+++ apps/client/src/views/Project.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import { AuthorCard } from "@/components/author/AuthorCard";
 import { Project } from "@/components/project/Project";
 import { Stats } from "@/components/project/Stats";
+import { Spinner } from "@/components/Spinner";
 import { MainLayout } from "@/layouts/MainLayout";
 import { api } from "@/utils/api";
 import { ParamsWithSlug } from "@/utils/types";
@@ -13,7 +14,7 @@ export function ProjectView() {
 
   const { slug } = router.query as ParamsWithSlug;
 
-  const { data } = api.project.useOne(
+  const { data, isLoading } = api.project.useOne(
     { slug },
     {
       enabled: router.isReady,
@@ -24,17 +25,20 @@ export function ProjectView() {
   return (
     <MainLayout>
       <Container size="lg" mt={20} pb={20}>
-        <Grid gutter={10}>
-          <Grid.Col span={1}>
-            <Stats />
-          </Grid.Col>
-          <Grid.Col span={8}>
-            <Project project={data} />
-          </Grid.Col>
-          <Grid.Col span={3}>
-            <AuthorCard project={data} />
-          </Grid.Col>
-        </Grid>
+        {data && (
+          <Grid gutter={10}>
+            <Grid.Col span={1}>
+              <Stats />
+            </Grid.Col>
+            <Grid.Col span={8}>
+              <Project project={data} />
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <AuthorCard project={data} />
+            </Grid.Col>
+          </Grid>
+        )}
+        {isLoading && <Spinner />}
       </Container>
     </MainLayout>
   );
