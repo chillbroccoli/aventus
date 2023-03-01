6e79398f847f91b5455dbfe201c1c9cb118aeaf5
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-01 18:41:26 +0100

    replace findById, add getUserDetails and updateUserDetails services

--- apps/server/src/models/user/user.service.ts
+++ apps/server/src/models/user/user.service.ts
@@ -1,17 +1,9 @@
 import * as argon2 from "argon2";
-import { CreateUserInput } from "shared";
+import { CreateUserInput, UpdateUserProfileInput } from "shared";
 
 import { prisma } from "../../utils/db";
 
 export const UserService = {
-  findById: async (id: string) => {
-    return prisma.user.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
-  },
-
   findAll: async () => {
     return prisma.user.findMany();
   },
@@ -21,16 +13,62 @@ export const UserService = {
 
     const hashedPassword = await argon2.hash(password);
 
-    return prisma.user.create({
+    const user = prisma.user.create({
       data: {
         ...rest,
         password: hashedPassword,
       },
     });
+
+    return user;
+  },
+
+  getUserDetails: async (id: string) => {
+    return prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        avatar: true,
+        bio: true,
+        location: true,
+        websiteUrl: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  },
+
+  updateUserDetails: async (
+    input: UpdateUserProfileInput & { userId: string }
+  ) => {
+    const { userId, ...rest } = input;
+
+    const user = await prisma.user.update({
+      where: {
+        id: parseInt(userId),
+      },
+      data: {
+        ...rest,
+      },
+    });
+
+    return user;
   },
 
   deleteOne: async (id: string) => {
     return await prisma.user.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+  },
+
+  findById: async (id: string) => {
+    return prisma.user.findUnique({
       where: {
         id: parseInt(id),
       },
