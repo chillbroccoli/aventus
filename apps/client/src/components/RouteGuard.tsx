4669d8fc612f3751c44471f926f4b545f243123a
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-01 22:12:13 +0100

    add RouteGuard component

---
+++ apps/client/src/components/RouteGuard.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { ClientRoutes, PROTECTED_ROUTES } from "shared";
+
+import { LoaderLayout } from "@/layouts/LoaderLayout";
+import { api } from "@/utils/api";
+
+export function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  const { data: user, isLoading } = api.user.useUserDetails();
+
+  if (isLoading) return <LoaderLayout />;
+
+  if (!user && PROTECTED_ROUTES.includes(router.pathname as ClientRoutes)) {
+    router.push(ClientRoutes.LOGIN);
+    return null;
+  }
+
+  return <>{children}</>;
+}
