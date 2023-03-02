94a9cacfd68ba3807eeb34eb674fe4ca637c344f
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-02 09:18:35 +0100

    update seeder

--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -1,8 +1,10 @@
+import { faker } from "@faker-js/faker";
 import { PrismaClient } from "@prisma/client";
+import * as argon2 from "argon2";
 
 const prisma = new PrismaClient();
 
-export const tags = [
+export const tagList = [
   "javascript",
   "typescript",
   "react",
@@ -76,11 +78,13 @@ export const tags = [
 ];
 
 export const seed = async () => {
+  const hashedPassword = await argon2.hash("password");
+
   await prisma.tag.deleteMany();
   await prisma.user.deleteMany();
   await prisma.project.deleteMany();
 
-  const createTags = tags.map((tag) =>
+  const createTags = tagList.map((tag) =>
     prisma.tag.create({
       data: {
         name: tag,
@@ -88,7 +92,87 @@ export const seed = async () => {
     })
   );
 
-  await prisma.$transaction(createTags);
+  const tags = await prisma.$transaction(createTags);
+
+  const userData = Array(10)
+    .fill(null)
+    .map(() => ({
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: hashedPassword,
+      avatar: faker.internet.avatar(),
+    }));
+
+  const createUsers = userData.map((user) =>
+    prisma.user.create({
+      data: user,
+    })
+  );
+
+  const users = await prisma.$transaction(createUsers);
+
+  const projects = [];
+
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < 3; j++) {
+      projects.push({
+        title: faker.commerce.productName(),
+        description: faker.lorem.paragraph(8),
+        content: faker.lorem.paragraphs(5),
+        slug: faker.lorem.slug(),
+        user: {
+          connect: {
+            id: users[i].id,
+          },
+        },
+        // Connect 3 random tags to each project
+        tags: {
+          connect: [
+            {
+              id: faker.datatype.number({
+                min: tags[0].id,
+                max: tags.slice(-1)[0].id,
+              }),
+            },
+            {
+              id: faker.datatype.number({
+                min: tags[0].id,
+                max: tags.slice(-1)[0].id,
+              }),
+            },
+            {
+              id: faker.datatype.number({
+                min: tags[0].id,
+                max: tags.slice(-1)[0].id,
+              }),
+            },
+          ],
+        },
+      });
+    }
+  }
+
+  const createProjects = projects.map((project) =>
+    prisma.project.create({
+      data: project,
+    })
+  );
+
+  await prisma.$transaction(createProjects);
+
+  await prisma.user.upsert({
+    where: {
+      email: "user@example.com",
+    },
+    update: {},
+    create: {
+      name: "johndoe",
+      email: "user@example.com",
+      password: hashedPassword,
+      role: "USER",
+      avatar: faker.internet.avatar(),
+    },
+  });
 
   await prisma.user.upsert({
     where: {
@@ -98,8 +182,7 @@ export const seed = async () => {
     create: {
       name: "admin",
       email: "admin@example.com",
-      password:
-        "$argon2i$v=19$m=16,t=2,p=1$QjVPd25hZ3VBWVVENktMMg$Lyuxbq0Qa3c0H1t+RGtM2A",
+      password: hashedPassword,
       role: "ADMIN",
       avatar:
         "https://pbs.twimg.com/profile_images/1498042411444051975/JLTc5ngd_400x400.jpg",
