379168270cb3f2f486499249ddd003378fd4569f
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-02 19:56:07 +0100

    add NotFoundState component

---
+++ apps/client/src/components/project/NotFoundState.tsx
@@ -0,0 +1,15 @@
+import { Alert } from "@mantine/core";
+import { IconMoodSad } from "@tabler/icons-react";
+
+export function NotFoundState() {
+  return (
+    <Alert
+      variant="filled"
+      mt={20}
+      icon={<IconMoodSad size={20} />}
+      title="That's it"
+    >
+      No more projects to load
+    </Alert>
+  );
+}
