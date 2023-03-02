181585fd8a99b6de3e71070707e3251c908fd680
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-02 19:57:41 +0100

    add error handling for mutations, add default options

--- apps/client/src/utils/queryClient.ts
+++ apps/client/src/utils/queryClient.ts
@@ -1,20 +1,51 @@
 import { showNotification } from "@mantine/notifications";
-import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 
 import { RequestError } from "./api/Fetcher";
 
 export const queryCache = new QueryCache({
   onError: (error: unknown) => {
-    if (error instanceof RequestError) {
-      showNotification({
+    if (error instanceof RequestError || error instanceof Error) {
+      return showNotification({
         title: "Error",
         message: error.message,
         color: "red",
       });
     }
+
+    return showNotification({
+      title: "Error",
+      message: "Something went wrong",
+      color: "red",
+    });
+  },
+});
+
+export const mutationCache = new MutationCache({
+  onError: (error: unknown) => {
+    if (error instanceof RequestError || error instanceof Error) {
+      return showNotification({
+        title: "Error",
+        message: error.message,
+        color: "red",
+      });
+    }
+
+    return showNotification({
+      title: "Error",
+      message: "Something went wrong",
+      color: "red",
+    });
   },
 });
 
 export const queryClient = new QueryClient({
   queryCache,
+  mutationCache,
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
