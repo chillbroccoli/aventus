e43f860e74e609229c59cc4a135fec9277eb3576
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-04 15:32:14 +0100

    fix typos

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class _Document extends Document {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
-          content="Aventus, an application where you can post you project ideas, project ideas for your or someone's next project, made for all of us developers, that cannot come up with idea what to do, what to build."
+          content="Aventus, an application where you can post your project ideas, project ideas for you or someone's next project, made for all of us developers, that cannot come up with idea what to do, what to build."
         />
         <body>
           <Main />
