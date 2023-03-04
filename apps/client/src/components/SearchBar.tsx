819bdcc6d6ddb0d2a220c26af238bea7f73ee415
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-04 03:27:37 +0100

    handle search state, navigate to search page

--- apps/client/src/components/SearchBar.tsx
+++ apps/client/src/components/SearchBar.tsx
@@ -1,15 +1,30 @@
 import { Box, TextInput, useMantineTheme } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { ClientRoutes } from "shared";
 
 export function SearchBar() {
   const theme = useMantineTheme();
+  const router = useRouter();
+
+  const [query, setQuery] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`${ClientRoutes.SEARCH}?q=${query}`);
+  };
 
   return (
     <Box>
-      <TextInput
-        placeholder="Search..."
-        rightSection={<IconSearch size={20} color={theme.colors.gray[5]} />}
-      />
+      <form onSubmit={onSubmit}>
+        <TextInput
+          placeholder="Search..."
+          rightSection={<IconSearch size={20} color={theme.colors.gray[5]} />}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
     </Box>
   );
 }
